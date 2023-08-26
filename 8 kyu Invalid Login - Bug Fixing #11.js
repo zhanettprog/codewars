d87ed5b3c036d18f6d7cfd6fb2a278a554beb8cf
Zhanett Serikbayeva <109654778+zhanettprog@users.noreply.github.com>
2023-08-26 14:17:21 +0600

    Add files via upload

---
+++ 8 kyu Invalid Login - Bug Fixing #11.js
@@ -0,0 +1,7 @@
+function validate(username, password) {
+  if (password.includes("||") || password.includes("//")) {
+    return "Wrong username or password!";
+  }
+  var database = new Database();
+  return database.login(username, password);
+}
